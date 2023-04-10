0311ed25980161c9f618d9d1d9136e4d1dd6a946
Michael Merimee <michaeltmerimee@gmail.com>
2023-04-10 16:02:23 -0400

    the first little bits of code for our capstone planning day

---
+++ java/src/main/java/com/techelevator/controller/TournamentController.java
@@ -0,0 +1,16 @@
+package com.techelevator.controller;
+
+public class TournamentController {
+
+    // POST A NEW TOURNAMENT
+
+    // EDIT A TOURNAMENT
+
+    // GET LIST OF ALL TOURNAMENTS
+
+    // GET LIST OF PAST TOURNAMENTS
+
+    // GET LIST OF PRESENT AND FUTURE TOURNAMENTS
+
+    // GET TOURNAMENT DETAILS
+}
